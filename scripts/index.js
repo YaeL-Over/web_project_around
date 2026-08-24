81dbe647a9927f4a6677281723841d869573d934
Yael <alfredo.ramos1693@gmail.com>
2026-08-23 19:24:38 -0600

    Sprint10v1.1

--- scripts/index.js
+++ scripts/index.js
@@ -45,8 +45,8 @@ const initialCards = [
 const gallery = document.querySelector(".gallery");
 
 // Popups
-const popupEdit = document.querySelector(".popup_edit");
-const popupAdd = document.querySelector(".main__profile-addbutton.popup");
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupAdd = document.querySelector(".popup_type_add");
 const popupImage = document.querySelector(".popup__image-p");
 const allPopups = document.querySelectorAll(".popup");
 
